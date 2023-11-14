e8508bc8e3e506608dee6e42ade98c56171ee058
Md Tarek Mahmud <mdtarekmahmud449@gmail.com>
2023-11-14 14:58:41 +0600

    initial

---
+++ app/src/main/java/com/example/funbook/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.funbook
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.funbook.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
