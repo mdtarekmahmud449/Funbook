27b0b8ae097d8dd168c1e14b69ae677887cbb5cc
Md Tarek Mahmud <mdtarekmahmud449@gmail.com>
2023-11-14 16:11:01 +0600

    Login & registratrion UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.funbook"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.funbook"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/funbook/RegistrationFragment.kt
+++ app/src/main/java/com/example/funbook/RegistrationFragment.kt
@@ -1,24 +1,16 @@
 package com.example.funbook
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.funbook.databinding.FragmentRegistrationBinding
 
-
-class RegistrationFragment : Fragment() {
-    private lateinit var binding: FragmentRegistrationBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentRegistrationBinding.inflate(inflater,  container, false)
-        return binding.root
-    }
-
+class RegistrationFragment : BaseFragment<FragmentRegistrationBinding> (FragmentRegistrationBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.alreadyLogin.setOnClickListener{
+            findNavController().popBackStack()
+        }
     }
 }
